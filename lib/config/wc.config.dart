ec477fc474b01e83a08a5e78bcfe06d8c7f7c339
ricarcoti <ricardo@pop-os.localdomain>
2020-11-14 18:00:31 -0500

    changed url in config

--- lib/config/wc.config.dart
+++ lib/config/wc.config.dart
@@ -1,7 +1,7 @@
 import 'package:woocommerce/woocommerce.dart';
 
 WooCommerce woocommerce = WooCommerce(
-  baseUrl: 'https://dev.bp-peru.com',
+  baseUrl: 'https://nk.cotillo.tech',
   consumerKey: 'ck_4850d5866bf4b56ce5b4ecbec860da423bc851af',
   consumerSecret: 'cs_70226b3d1420e52a51689fc32fcfe359e617b8e1',
   isDebug: true,
