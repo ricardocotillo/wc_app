69cb69db93a4f40b8b579defa0f54516dbe63fe2
ricarcoti <ricardo@pop-os.localdomain>
2020-11-15 18:50:01 -0500

    fnished order process

--- lib/views/checkout/orderInfo.view.dart
+++ lib/views/checkout/orderInfo.view.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:wc_app/views/home.view.dart';
 
 class OrderInfoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
-      body: Center(
-        child: Column(
-          children: [
-            Icon(Icons.check_box),
-            Text('Gracias, tu orden ha sido recibida'),
-          ],
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => HomeView(),
+          ));
+        },
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.check,
+                color: Colors.white,
+                size: 50,
+              ),
+              Text(
+                'Gracias, tu orden ha sido recibida.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Tap para regresar al inicio',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
